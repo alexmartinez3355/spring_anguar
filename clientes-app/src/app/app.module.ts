4a3c7c8cc1fd54598bb414f5a27677a498f088bb
Alexis Martinez <alex_martinez_3355@hotmail.com>
2020-03-15 16:38:18 -0600

    Agregando bootstrap de forma manual (por medio de BootstrapCDN). Se crearon algunos componentes tanto del header como del footer y un poco de contenido en forma de listas de objetos. Se uso por primera vez *ngFor y *ngIf con el ejemplo de mostrar y ocultar contenido mediante un botón.

---
+++ clientes-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivaComponent } from './components/directiva/directiva.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
