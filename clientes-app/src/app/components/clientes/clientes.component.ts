8bde3bbe078d5e81fb23f1c1f6fa93823335191c
Alexis Martinez <alex_martinez_3355@hotmail.com>
2020-03-16 17:44:26 -0600

    Generar tabla con registros a trabes de un archivo aparte en formato JSON en lugar de usar un arreglo en el componente.

--- clientes-app/src/app/components/clientes/clientes.component.ts
+++ clientes-app/src/app/components/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente.class';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,15 +8,11 @@ import { Cliente } from './cliente.class';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {cliente_id: 1, nombre: 'Alex', apellido: 'Martínez', email: 'alex@gmail.com', fecha: '2020'},
-    {cliente_id: 2, nombre: 'Araceli', apellido: 'Campos', email: 'araceli@gmail.com', fecha: '2020'},
-    {cliente_id: 3, nombre: 'Valentina', apellido: 'Martínez', email: 'vale_m@gmail.com', fecha: '2020'},
-    {cliente_id: 4, nombre: 'Efren', apellido: 'Martínez', email: 'efren@gmail.com', fecha: '2020'}
-  ];
+  clientes: Cliente[];
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ clientes-app/src/app/components/clientes/clientes.json.ts
@@ -0,0 +1,7 @@
+import { Cliente } from './cliente.class';
+export const CLIENTES: Cliente[] = [
+    {cliente_id: 1, nombre: 'Alex', apellido: 'Martínez', email: 'alex@gmail.com', fecha: '2020'},
+    {cliente_id: 2, nombre: 'Araceli', apellido: 'Campos', email: 'araceli@gmail.com', fecha: '2020'},
+    {cliente_id: 3, nombre: 'Valentina', apellido: 'Martínez', email: 'vale_m@gmail.com', fecha: '2020'},
+    {cliente_id: 4, nombre: 'Efren', apellido: 'Martínez', email: 'efren@gmail.com', fecha: '2020'}
+  ]
