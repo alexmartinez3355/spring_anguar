b722e1b614f4cf5e448952317d62d33273fca147
Alexis Martinez <alex_martinez_3355@hotmail.com>
2020-03-16 17:30:39 -0600

    Generación de una tabla de clientes con el elemento Card de bootstrap y la sentencia gnFor.

--- clientes-app/src/app/components/clientes/clientes.component.ts
+++ clientes-app/src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente.class';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Cliente[] = [
+    {cliente_id: 1, nombre: 'Alex', apellido: 'Martínez', email: 'alex@gmail.com', fecha: '2020'},
+    {cliente_id: 2, nombre: 'Araceli', apellido: 'Campos', email: 'araceli@gmail.com', fecha: '2020'},
+    {cliente_id: 3, nombre: 'Valentina', apellido: 'Martínez', email: 'vale_m@gmail.com', fecha: '2020'},
+    {cliente_id: 4, nombre: 'Efren', apellido: 'Martínez', email: 'efren@gmail.com', fecha: '2020'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
